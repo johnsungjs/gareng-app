5e5cd4bb4c6137bc30fd6e88a1d48b0b4af714ec
John Sung <johnsungjs@gmail.com>
2023-04-25 17:01:20 +0700

    carousel done

--- gareng-front/gareng_front/lib/pages/HomePage.dart
+++ gareng-front/gareng_front/lib/pages/HomePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:gareng_front/widgets/Carousel.dart';
 import 'package:gareng_front/widgets/CustomCard.dart';
 import 'package:gareng_front/widgets/SearchBar.dart';
 
@@ -9,85 +10,76 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: [
-        // Text("3 logo atassssssssssssssssssssssssssssssss"),
-        SizedBox(
-          height: 20,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Column(children: [
+      // Text("3 logo atassssssssssssssssssssssssssssssss"),
+      SizedBox(
+        height: 20,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Logo"),
+          Text("Location"),
+          Text("Notif Logo"),
+        ],
+      ),
+      // Text("Carousel"),
+      SizedBox(
+        height: 20,
+      ),
+      Carousel(),
+      SizedBox(
+        height: 20,
+      ),
+      // Text("SearchBar"),
+      SearchBar(),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Popular Food"),
+          Text("View All"),
+        ],
+      ),
+      // Text("Grids Of Menu"),
+      Expanded(
+        child: GridView.count(
+          crossAxisCount: 2,
           children: [
-            Text("Logo"),
-            Text("Location"),
-            Text("Notif Logo"),
+            CustomCard(
+                title: "title1",
+                image:
+                    "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
+            CustomCard(
+                title: "title2",
+                image:
+                    "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
+            CustomCard(
+                title: "title3",
+                image:
+                    "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
+            CustomCard(
+                title: "title4",
+                image:
+                    "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
+            CustomCard(
+                title: "title4",
+                image:
+                    "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
+            CustomCard(
+                title: "title4",
+                image:
+                    "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
+            CustomCard(
+                title: "title4",
+                image:
+                    "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
+            CustomCard(
+                title: "title4",
+                image:
+                    "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
           ],
         ),
-        // Text("Carousel"),
-        SizedBox(
-          height: 20,
-        ),
-        Container(
-          width: 300,
-          height: 150,
-          decoration: BoxDecoration(
-              border: Border.all(), borderRadius: BorderRadius.circular(20)),
-          child: Text("Carousel Here"),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        // Text("SearchBar"),
-        SearchBar(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("Popular Food"),
-            Text("View All"),
-          ],
-        ),
-        // Text("Grids Of Menu"),
-        Expanded(
-          child: GridView.count(
-            crossAxisCount: 2,
-            children: [
-              CustomCard(
-                  title: "title1",
-                  image:
-                      "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
-              CustomCard(
-                  title: "title2",
-                  image:
-                      "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
-              CustomCard(
-                  title: "title3",
-                  image:
-                      "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
-              CustomCard(
-                  title: "title4",
-                  image:
-                      "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
-              CustomCard(
-                  title: "title4",
-                  image:
-                      "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
-              CustomCard(
-                  title: "title4",
-                  image:
-                      "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
-              CustomCard(
-                  title: "title4",
-                  image:
-                      "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
-              CustomCard(
-                  title: "title4",
-                  image:
-                      "https://thumbs.dreamstime.com/b/toyota-ae-diecast-car-model-isolated-white-background-toyota-ae-249136538.jpg"),
-            ],
-          ),
-        ),
-      ]),
-    );
+      ),
+    ]);
   }
 }
---
+++ gareng-front/gareng_front/lib/widgets/Carousel.dart
@@ -0,0 +1,112 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Carousel extends StatefulWidget {
+  const Carousel({super.key});
+
+  @override
+  State<Carousel> createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
+  late final PageController pageController;
+  int pageNo = 0;
+
+  Timer? carouselTimer;
+
+  Timer getTimer() {
+    return Timer.periodic(const Duration(seconds: 3), (timer) {
+      if (pageNo == 5) {
+        pageNo = 0;
+      }
+      pageController.animateToPage(pageNo,
+          duration: const Duration(seconds: 1), curve: Curves.easeInOutCirc);
+      pageNo++;
+    });
+  }
+
+  @override
+  void initState() {
+    pageController = PageController(initialPage: 0, viewportFraction: 0.85);
+    carouselTimer = getTimer();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 200,
+          child: PageView.builder(
+            controller: pageController,
+            onPageChanged: (index) {
+              pageNo = index;
+              setState(() {});
+            },
+            itemBuilder: (_, index) {
+              return AnimatedBuilder(
+                animation: pageController,
+                builder: (ctx, child) {
+                  return child!;
+                },
+                child: GestureDetector(
+                  onTap: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("you clicked index no $index"),
+                      ),
+                    );
+                  },
+                  onPanDown: (d) {
+                    carouselTimer?.cancel();
+                    carouselTimer = null;
+                  },
+                  onPanCancel: () {
+                    carouselTimer = getTimer();
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                        right: 8, left: 8, top: 36, bottom: 12),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(24),
+                        color: Colors.amberAccent),
+                  ),
+                ),
+              );
+            },
+            itemCount: 5,
+          ),
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+                5,
+                (index) => GestureDetector(
+                      child: Container(
+                        margin: EdgeInsets.all(2),
+                        child: Icon(
+                          Icons.circle,
+                          size: 12,
+                          color: pageNo == index
+                              ? Colors.indigoAccent
+                              : Colors.grey.shade300,
+                        ),
+                      ),
+                    )))
+      ],
+    );
+  }
+}
